6041fc9df839a29380a8da644c49a573193bc43c
chaitra3456 <chaitra2468chai@gmail.com>
2024-09-10 15:59:28 +0530

    Add files via upload

---
+++ digit number.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main (){
+ int n,sum=0;
+  printf("Enter the number");
+  scanf("%d",&n );
+  while(n!=0){
+    sum=sum+(n%10);
+    n=n/10;
+  }
+  printf("sum of digit is %d",sum);
+}
